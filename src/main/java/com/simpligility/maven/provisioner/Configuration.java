42837e25d696c931d6f9f4d6659b29856914bbb3
Manfred Moser <manfred@simpligility.com>
2014-08-05 16:52:24 -0700

    JCommander based configuration file and first cut of a working provisioner class

---
+++ src/main/java/com/simpligility/maven/provisioner/Configuration.java
@@ -0,0 +1,58 @@
+package com.simpligility.maven.provisioner;
+
+import java.io.File;
+
+import com.beust.jcommander.Parameter;
+
+public class Configuration {
+  
+  @Parameter(names = "-help", help = true)
+  private boolean help;
+
+  
+  @Parameter(names = {"-s", "-sourceUrl" },
+             description = "URL for the source repository from which artifacts "
+                 + "are resolved, defaults to https://repo1.maven.org/maven2, "
+                 + "example for a Nexus isntall is "
+                 + "http://localhost:8081/nexus/content/groups/public  "
+             )
+  private String sourceUrl = "https://repo1.maven.org/maven2";
+
+  @Parameter(names = {"-t", "-targetUrl"}, 
+             description = "Folder or URL for the target repository e.g. dist-repo "
+                 + "or http://localhost:8081/nexus/content/repositories/release", 
+             required = true)
+  private String targetUrl; 
+
+  @Parameter(names = {"-a", "-artifactCoordinates"}, 
+             description = "GAV coordinate of the desired artifact in the "
+                 + "syntax groupId:artifactId:version e.g. "
+                 + "org.apache.commons:commons-lang3:3.3.2",
+             required = true) 
+  private String artifactCoordinate;
+  
+  @Parameter(names = {"-u", "-username"},
+             description = "Username for the deployment, if required.")
+  private String username;
+  
+  @Parameter(names = {"-p", "-password"},
+  description = "Password for the deployment, if required.")
+  private String password;
+
+  public String getSourceUrl() {
+    return sourceUrl;
+  }
+  public String getTargetUrl() {
+    
+    if (!targetUrl.startsWith("http")) {
+      //if url does not start with http (or https..) we assume it is a file path and convert it with 
+      return new File( targetUrl ).toURI().toString();
+    } else {
+      return targetUrl;
+    }
+    
+  }
+  public String getArtifactCoordinate() {
+    return artifactCoordinate;
+  }
+}
--- src/main/java/com/simpligility/maven/provisioner/MavenRepositoryProvisioner.java
+++ src/main/java/com/simpligility/maven/provisioner/MavenRepositoryProvisioner.java
@@ -3,67 +3,123 @@
  */
 package com.simpligility.maven.provisioner;
 
+import java.io.File;
 import java.util.List;
 
+import org.apache.http.client.HttpResponseException;
 import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.collection.CollectRequest;
+import org.eclipse.aether.deployment.DeployRequest;
+import org.eclipse.aether.deployment.DeploymentException;
 import org.eclipse.aether.examples.util.Booter;
 import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.graph.DependencyFilter;
+import org.eclipse.aether.repository.Authentication;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.resolution.ArtifactResult;
 import org.eclipse.aether.resolution.DependencyRequest;
 import org.eclipse.aether.resolution.DependencyResolutionException;
+import org.eclipse.aether.transfer.ArtifactTransferException;
 import org.eclipse.aether.util.artifact.JavaScopes;
+import org.eclipse.aether.util.artifact.SubArtifact;
 import org.eclipse.aether.util.filter.DependencyFilterUtils;
+import org.eclipse.aether.util.repository.AuthenticationBuilder;
+
+import com.beust.jcommander.JCommander;
 
 /**
- * @author Manfred Moser
+ * MavenRepositoryProvisioner
+ * 
+ * @author Manfred Moser <manfred@simpligility.com>
  */
 public class MavenRepositoryProvisioner {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+  private static RepositorySystem system;
+  private static RepositorySystemSession session;
 
-		RepositorySystem system = Booter.newRepositorySystem();
+  private static Configuration config;
 
-		RepositorySystemSession session = Booter
-				.newRepositorySystemSession(system);
+  public static void main(String[] args) {
 
-		Artifact artifact = new DefaultArtifact(
-				"org.sonatype.aether:aether-impl:1.13");
+    JCommander jcommander = null;
+    Boolean validConfig = false;
+    
+    config = new Configuration();
+    try {
+      jcommander = new JCommander(config, args);
+      validConfig = true;
+    } catch (Exception e) {
+      jcommander.usage();
+    }
+    
+    if (validConfig) {
+      system = Booter.newRepositorySystem();
+      session = Booter.newRepositorySystemSession(system);
+  
+      Artifact artifact = new DefaultArtifact(config.getArtifactCoordinate());
+  
+      RemoteRepository repo = new RemoteRepository.Builder("central", "default",
+          config.getSourceUrl()).build();
+  
+      DependencyFilter depFilter = DependencyFilterUtils
+          .classpathFilter(JavaScopes.COMPILE);
+  
+      CollectRequest collectRequest = new CollectRequest();
+      collectRequest.setRoot(new Dependency(artifact, JavaScopes.COMPILE));
+      collectRequest.addRepository(repo);
+  
+      DependencyRequest dependencyRequest = new DependencyRequest(collectRequest,
+          depFilter);
+  
+      List<ArtifactResult> artifactResults = null;
+      try {
+        artifactResults = system.resolveDependencies(session, dependencyRequest)
+            .getArtifactResults();
+      } catch (DependencyResolutionException e) {
+        // TODO log error
+        e.printStackTrace();
+      }
+      if (artifactResults != null) {
+  
+        for (ArtifactResult artifactResult : artifactResults) {
+          System.out.println(artifactResult.getArtifact() + " resolved to "
+              + artifactResult.getArtifact().getFile());
+          try {
+            deployArtifactResult(artifactResult);
+          } catch (ArtifactTransferException ate) {
+            System.out.println("ate");
+          } catch (HttpResponseException hre) {
+            System.out.println("hre");
+          } catch (DeploymentException de) {
+            System.out.println("de");
+          }
+        }
+      }
+    }
+  }
 
-		RemoteRepository repo = Booter.newCentralRepository();
+  private static void deployArtifactResult(ArtifactResult artifactResult)
+      throws HttpResponseException, ArtifactTransferException,
+      DeploymentException {
+    Artifact jarArtifact = artifactResult.getArtifact();
 
-		DependencyFilter classpathFlter = DependencyFilterUtils
-				.classpathFilter(JavaScopes.COMPILE);
+    Artifact pomArtifact = new SubArtifact(jarArtifact, "", "pom");
+    pomArtifact = pomArtifact.setFile(new File("pom.xml"));
 
-		CollectRequest collectRequest = new CollectRequest();
-		collectRequest.setRoot(new Dependency(artifact, JavaScopes.COMPILE));
-		collectRequest.addRepository(repo);
+    RemoteRepository distRepo = new RemoteRepository.Builder(
+        "repositoryIdentifier", "default", config.getTargetUrl())
+        .setAuthentication(
+            new AuthenticationBuilder().addUsername("admin")
+                .addPassword("admin123").build()).build();
 
-		DependencyRequest dependencyRequest = new DependencyRequest(
-				collectRequest, classpathFlter);
+    DeployRequest deployRequest = new DeployRequest();
+    deployRequest.addArtifact(jarArtifact).addArtifact(pomArtifact);
+    deployRequest.setRepository(distRepo);
 
-		List<ArtifactResult> artifactResults = null;
-		try {
-			artifactResults = system.resolveDependencies(
-					session, dependencyRequest).getArtifactResults();
-		} catch (DependencyResolutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		if (artifactResults != null) {
-		
-			for (ArtifactResult artifactResult : artifactResults) {
-				System.out.println(artifactResult.getArtifact() + " resolved to "
-						+ artifactResult.getArtifact().getFile());
-			}
-		}
-
-	}
+    system.deploy(session, deployRequest);
+  }
 
 }
