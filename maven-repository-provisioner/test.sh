a0aca119ff103b0f88095b970c51d3925167041a
Manfred Moser <manfred@simpligility.com>
2014-10-29 15:35:46 -0700

    minimal example/test script

---
+++ maven-repository-provisioner/test.sh
@@ -0,0 +1,1 @@
+java -jar target/maven-reposito*-with-dependencies.jar -s http://localhost:8081/content/groups/public -t http://localhost:8081/content/repositories/test -u admin -p admin123 -a junit:junit:4.11
