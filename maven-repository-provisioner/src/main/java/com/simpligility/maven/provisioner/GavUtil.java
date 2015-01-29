29ad4be10b07ec0164d358f4ca3e684ec2f8a774
Manfred Moser <manfred@simpligility.com>
2015-01-29 11:09:53 -0800

    using File.separator to make it work in Windows..

--- maven-repository-provisioner/src/main/java/com/simpligility/maven/provisioner/GavUtil.java
+++ maven-repository-provisioner/src/main/java/com/simpligility/maven/provisioner/GavUtil.java
@@ -1,19 +1,21 @@
 package com.simpligility.maven.provisioner;
 
+import java.io.File;
+
 public class GavUtil
 {
 
   public static Gav getGavFromRepositoryPath(String leafRepoPath) {
 
-    int versionStartSlash = leafRepoPath.lastIndexOf( '/' );
+    int versionStartSlash = leafRepoPath.lastIndexOf( File.separator );
     String version = leafRepoPath.substring( versionStartSlash + 1 , leafRepoPath.length() );
 
     String gaPath = leafRepoPath.substring( 0 , versionStartSlash );
-    int gaStartSlash = gaPath.lastIndexOf( '/' );
+    int gaStartSlash = gaPath.lastIndexOf( File.separator );
     String artifactId = gaPath.substring(  gaStartSlash + 1, gaPath.length() );
     
     String gPath = gaPath.substring( 0 , gaStartSlash );
-    String groupId = gPath.replace( '/', '.' );
+    String groupId = gPath.replace( File.separator, "." );
     
     return new Gav(groupId, artifactId, version);
   }
