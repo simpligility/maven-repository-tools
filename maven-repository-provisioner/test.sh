3f8d04401f0785371296a88727c8633296350241
Manfred Moser <manfred@simpligility.com>
2016-04-18 14:55:37 -0700

    changed to jsut commons-lang test for now

--- maven-repository-provisioner/test.sh
+++ maven-repository-provisioner/test.sh
@@ -15,15 +15,15 @@ function deploy {
   java -jar target/maven-reposito*-with-dependencies.jar -s $source  -t $target -u admin -p admin123 -a $1
 }
 
-deploy "junit:junit:4.11|org.testng:testng:6.9.10|com.squareup.assertj:assertj-android:aar:1.0.0"
+# deploy "junit:junit:4.11|org.testng:testng:6.9.10|com.squareup.assertj:assertj-android:aar:1.0.0"
 
 # deploy "org.apache.maven.plugins:maven-surefire-plugin:jar:2.18.1"
 
- deploy "org.apache.commons:commons-lang3:jar:3.3.2"
+deploy "org.apache.commons:commons-lang3:jar:3.3.2"
 
 # deploy "com.google.inject:guice:no_aop:jar:3.0"
 
 # deploy "org.apache.commons:commons-lang3:jar:3.3.2|junit:junit:4.11|com.squareup.assertj:assertj-android:aar:1.0.0"
 
-deploy "com.simpligility.maven:progressive-organization-pom:pom:2.3.0"
+# deploy "com.simpligility.maven:progressive-organization-pom:pom:4.1.0"
 
