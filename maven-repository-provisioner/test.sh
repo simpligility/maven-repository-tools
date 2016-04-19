f3fd8aa572dd5d830df9fca68be35c65cc7ab04f
Manfred Moser <manfred@simpligility.com>
2016-04-19 09:55:02 -0700

    final documentation updates for 1.0.0 release

--- maven-repository-provisioner/test.sh
+++ maven-repository-provisioner/test.sh
@@ -11,6 +11,9 @@ set -u
 source=http://localhost:8081/repository/maven-public
 target=http://localhost:8081/repository/tmp
 
+creds="-u admin -p admin123"
+
+
 function deploy {
   if [ -n "$1" ]; then
     java -jar target/maven-reposito*-with-dependencies.jar -s $source  -t $target -u admin -p admin123 -a $1
@@ -23,14 +26,16 @@ function deploy {
 
 # deploy "org.apache.maven.plugins:maven-surefire-plugin:jar:2.18.1"
 
-deploy "org.apache.commons:commons-lang3:jar:3.3.2"
+#deploy "org.apache.commons:commons-lang3:jar:3.3.2"
 
-deploy "com.google.inject:guice:no_aop:jar:3.0"
+#deploy "com.google.inject:guice:no_aop:jar:3.0"
 
 # deploy "org.apache.commons:commons-lang3:jar:3.3.2|junit:junit:4.11|com.squareup.assertj:assertj-android:aar:1.0.0"
 
-deploy "com.simpligility.maven:progressive-organization-pom:pom:4.1.0"
+#deploy "com.simpligility.maven:progressive-organization-pom:pom:4.1.0"
 
 # testing repo folder transfer only
 #deploy ""
 
+
+java -jar target/maven-repository-provisioner-*-jar-with-dependencies.jar  -cd "local-cache" -t $target $creds
