b9a1e07d604cfb750da788862d6b9139e809af28
Manfred Moser <manfred@simpligility.com>
2017-06-28 17:19:33 -0700

    added another bundle example, changed to use WaRM url target

--- maven-repository-provisioner/test.sh
+++ maven-repository-provisioner/test.sh
@@ -4,9 +4,15 @@ set -e
 # fail if a function call is missing an argument
 set -u
 
+# Central Repository directly
+source=https://repo1.maven.org/maven2/
+
+# for my local WaRM
+#source=http://localhost:8081/content/groups/public
+target=http://localhost:8081/content/repositories/test
 # for my local Nexus
-source=http://localhost:8081/nexus/content/groups/public
-target=http://localhost:8081/nexus/content/repositories/test
+#source=http://localhost:8081/nexus/content/groups/public
+#target=http://localhost:8081/nexus/content/repositories/test
 # for my local Nexus 3
 #source=http://localhost:8081/repository/maven-public
 #target=http://localhost:8081/repository/tmp
@@ -40,10 +46,10 @@ function deploy {
 #deploy ""
 
 # testing OSGI bundle packaging and related .jar transfer
-# deploy org.apache.geronimo.specs:geronimo-ejb_3.1_spec:1.0.2
-
+#deploy org.apache.geronimo.specs:geronimo-ejb_3.1_spec:1.0.2
+deploy org.drools:drools-compiler:6.5.0.Final
 
 # test for including provided scope
-deploy com.hazelcast:hazelcast:3.7.2
+#deploy com.hazelcast:hazelcast:3.7.2
 
 #java -jar target/maven-repository-provisioner-*-jar-with-dependencies.jar  -cd "local-cache" -t $target $creds
