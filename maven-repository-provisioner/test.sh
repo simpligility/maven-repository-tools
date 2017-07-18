8dcb42a5c47b42efd6c3e258e868beaee6c41c11
Manfred Moser <manfred@simpligility.com>
2017-07-18 14:36:39 -0700

    simplified and cleaned up test script, added docs

--- maven-repository-provisioner/test.sh
+++ maven-repository-provisioner/test.sh
@@ -29,32 +29,43 @@ function deploy {
   fi
 }
 
-#deploy "junit:junit:4.11|org.testng:testng:6.9.10|com.squareup.assertj:assertj-android:aar:1.0.0"
+# normal JAR
 
-#deploy "org.apache.commons:commons-lang3:jar:3.3.2"
+#deploy "junit:junit:4.11"
+#deploy "org.testng:testng:jar:6.9.10"
+deploy "org.apache.commons:commons-lang3:jar:3.3.2"
+#deploy "org.apache.abdera:abdera-bundle:1.1.3"
+#deploy "com.google.inject:guice:jar:no_aop:3.0"
+#deploy "org.apache.commons:commons-lang3:jar:3.3.2|junit:junit:4.11|com.squareup.assertj:assertj-android:aar:1.1.1"
 
-# deploy "org.apache.abdera:abdera-bundle:1.1.3" 
-
-#deploy "com.google.inject:guice:no_aop:jar:3.0"
-
-# deploy "org.apache.commons:commons-lang3:jar:3.3.2|junit:junit:4.11|com.squareup.assertj:assertj-android:aar:1.0.0"
-
-# testing pom packaging
+# POM packaging
 #deploy "com.simpligility.maven:progressive-organization-pom:pom:4.1.0"
 
+# AAR
+
+# should download aar and jar
+#deploy "com.squareup.assertj:assertj-android:aar:1.1.1"
+# however this will NOT work since the pom file uses jar packaging so only jar
+# is downloaded - there is no way to tell this is actually an aar as well...
+# deploy "com.squareup.assertj:assertj-android:1.1.1"
+
+
 # testing OSGI bundle packaging and related .jar transfer
+# should always download jar and NOT cause a failure
 #deploy org.apache.geronimo.specs:geronimo-ejb_3.1_spec:1.0.2
+#deploy org.apache.geronimo.specs:geronimo-ejb_3.1_spec:bundle:1.0.2
 #deploy org.drools:drools-compiler:6.5.0.Final
+#deploy org.drools:drools-compiler:bundle:6.5.0.Final
 #deploy org.kie:kie-api:bundle:6.5.0.Final
 #deploy org.kie:kie-api:6.5.0.Final
 
-# testing hpi packaging
+# testing hpi packaging, both should get a hpi file and a jar file
 #deploy org.jenkins-ci.plugins:git:hpi:3.4.0
-deploy org.jenkins-ci.plugins:git:3.4.0
+#deploy org.jenkins-ci.plugins:git:3.4.0
 
 # testing maven-plugin packaging
-# deploy "org.apache.maven.plugins:maven-surefire-plugin:jar:2.18.1"
-# deploy "org.apache.maven.plugins:maven-surefire-plugin:maven-plugin:2.18.1"
+#deploy "org.apache.maven.plugins:maven-surefire-plugin:jar:2.18.1"
+#deploy "org.apache.maven.plugins:maven-surefire-plugin:maven-plugin:2.18.1"
 
 # test for including provided scope
 #deploy com.hazelcast:hazelcast:3.7.2
