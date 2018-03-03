82b96922a854634f87343dbb6730e14c8e07fc85
Manfred Moser <manfred@simpligility.com>
2018-03-03 07:03:52 -0800

    extracted a few methods

--- maven-repository-provisioner/src/main/java/com/simpligility/maven/provisioner/MavenRepositoryProvisioner.java
+++ maven-repository-provisioner/src/main/java/com/simpligility/maven/provisioner/MavenRepositoryProvisioner.java
@@ -67,60 +67,11 @@ public class MavenRepositoryProvisioner
                 StringBuilder provisioningSuccessMessage = new StringBuilder();
                 logger.info( config.getConfigSummary() );
 
-                cacheDirectory = new File( config.getCacheDirectory() );
-                logger.info( " Absolute path: " + cacheDirectory.getAbsolutePath() );
-                if ( cacheDirectory.exists() && cacheDirectory.isDirectory() ) 
-                {
-                  logger.info( "Detected local cache directory '" + config.getCacheDirectory() + "'." );
-                  if ( !config.hasArtifactsCoordinates() )
-                  {
-                    logger.info( "No artifact coordinates specified - using cache directory as source." );
-                  } 
-                  else
-                  {
-                    logger.info( "Artifact coordinates specified "
-                        + "- removing stale cache directory from prior execution." );
-                    try
-                    {
-                        FileUtils.deleteDirectory( cacheDirectory );
-                        logger.info( config.getCacheDirectory() + " deleted." );
-                    }
-                    catch ( IOException e )
-                    {
-                        logger.info( config.getCacheDirectory() + " deletion failed" );
-                        exitFailure( "Failed to delete stale cache directory." );
-                    }
-                    cacheDirectory = new File( config.getCacheDirectory() );
-                    cacheDirectory.mkdirs();
-                  }
-                }
-                else
-                {
-                  cacheDirectory = new File( config.getCacheDirectory() );
-                  cacheDirectory.mkdirs();
-                }
-                ArtifactRetriever retriever = null;
-                if ( config.hasArtifactsCoordinates() ) 
-                {
-                  logger.info( "Artifact retrieval starting." );
-                  retriever = new ArtifactRetriever( cacheDirectory );
-                  retriever.retrieve( config.getArtifactCoordinates(), config.getSourceUrl(), 
-                      config.getSourceUsername(), config.getSourcePassword(), 
-                      config.getIncludeSources(), config.getIncludeJavadoc(), 
-                      config.getIncludeProvidedScope(), config.getIncludeTestScope(), config.getIncludeRuntimeScope() );
+                prepareCacheDirectory();
 
-                  logger.info( "Artifact retrieval completed." );
-                } 
-                else 
-                {
-                  logger.info( "Artifact retrieval skipped. " );
-                }
+                ArtifactRetriever retriever = retrieveArtifacts();
 
-                logger.info( "Artifact deployment starting." );
-                MavenRepositoryDeployer helper = new MavenRepositoryDeployer( cacheDirectory );
-                helper.deployToRemote( config.getTargetUrl(), config.getUsername(), config.getPassword(), 
-                                       config.getCheckTarget(), config.getVerifyOnly() );
-                logger.info( "Artifact deployment completed." );
+                MavenRepositoryDeployer helper = deployArtifacts();
 
                 logger.info( "Processing Completed." );
                 StringBuilder summary = new StringBuilder();
@@ -168,6 +119,73 @@ public class MavenRepositoryProvisioner
                 }
             }
         }
+    }
+
+    private static MavenRepositoryDeployer deployArtifacts()
+    {
+      logger.info( "Artifact deployment starting." );
+      MavenRepositoryDeployer helper = new MavenRepositoryDeployer( cacheDirectory );
+      helper.deployToRemote( config.getTargetUrl(), config.getUsername(), config.getPassword(),
+                             config.getCheckTarget(), config.getVerifyOnly() );
+      logger.info( "Artifact deployment completed." );
+      return helper;
+    }
+
+    private static ArtifactRetriever retrieveArtifacts()
+    {
+      ArtifactRetriever retriever = null;
+      if ( config.hasArtifactsCoordinates() )
+      {
+        logger.info( "Artifact retrieval starting." );
+        retriever = new ArtifactRetriever( cacheDirectory );
+        retriever.retrieve( config.getArtifactCoordinates(), config.getSourceUrl(),
+            config.getSourceUsername(), config.getSourcePassword(),
+            config.getIncludeSources(), config.getIncludeJavadoc(),
+            config.getIncludeProvidedScope(), config.getIncludeTestScope(), config.getIncludeRuntimeScope() );
+
+        logger.info( "Artifact retrieval completed." );
+      }
+      else
+      {
+        logger.info( "Artifact retrieval skipped. " );
+      }
+      return retriever;
+    }
+
+    private static void prepareCacheDirectory()
+    {
+      cacheDirectory = new File( config.getCacheDirectory() );
+      logger.info( " Absolute path: " + cacheDirectory.getAbsolutePath() );
+      if ( cacheDirectory.exists() && cacheDirectory.isDirectory() )
+      {
+        logger.info( "Detected local cache directory '" + config.getCacheDirectory() + "'." );
+        if ( !config.hasArtifactsCoordinates() )
+        {
+          logger.info( "No artifact coordinates specified - using cache directory as source." );
+        }
+        else
+        {
+          logger.info( "Artifact coordinates specified "
+              + "- removing stale cache directory from prior execution." );
+          try
+          {
+              FileUtils.deleteDirectory( cacheDirectory );
+              logger.info( config.getCacheDirectory() + " deleted." );
+          }
+          catch ( IOException e )
+          {
+              logger.info( config.getCacheDirectory() + " deletion failed" );
+              exitFailure( "Failed to delete stale cache directory." );
+          }
+          cacheDirectory = new File( config.getCacheDirectory() );
+          cacheDirectory.mkdirs();
+        }
+      }
+      else
+      {
+        cacheDirectory = new File( config.getCacheDirectory() );
+        cacheDirectory.mkdirs();
+      }
     }
 
     private static void exitWithHelpMessage( String helpText ) 
